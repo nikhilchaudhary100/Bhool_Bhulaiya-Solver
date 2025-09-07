0f80bb3cdec53951f623980e5866857d5c54f2a5
nikhilchaudhary100 <nikhilchaudhary141180@gmail.com>
2025-09-07 10:30:37 +0530

    time.sleep(0.3).py

--- Shortest Path Finder.py
+++ Shortest Path Finder.py
@@ -4,16 +4,15 @@ import queue
 import time
 
 bhool_bhulaiya = [
-    ["|", "|", "|", "|", "|", "S", "|", "|", "|", "|"],
-    ["|", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
-    ["|", "|", "|", "|", "|", "|", "|", "|", " ", "|"],
-    ["|", " ", " ", " ", " ", " ", " ", "|", " ", "|"],
-    ["|", " ", "|", "|", "|", "|", " ", "|", " ", "|"],
-    ["|", " ", "|", " ", " ", " ", " ", "|", " ", "|"],
-    ["|", " ", "|", " ", "|", "|", " ", "|", " ", "|"],
-    ["|", " ", "|", " ", "|", " ", " ", " ", " ", "|"],
-    ["|", " ", "|", " ", "|", " ", "|", " ", "|", "|"],
-    ["|", "|", "|", "|", "|", "|", "|", "E", "|", "|"]
+    ["|", "|", "|", "|", "|", "S", "|", "|", "|"],
+    ["|", " ", " ", " ", "|", " ", " ", "|", "|"],
+    ["|", " ", " ", " ", "|", " ", " ", " ", "|"],
+    ["|", " ", "|", " ", "|", " ", "|", " ", "|"],
+    ["|", " ", "|", " ", " ", " ", "|", " ", "|"],
+    ["|", " ", "|", " ", "|", " ", "|", " ", "|"],
+    ["|", " ", "|", " ", "|", " ", "|", " ", "|"],
+    ["|", " ", "|", " ", "|", " ", "|", " ", "|"],
+    ["|", "E", "|", "|", "|", "|", "|", "|", "|"]
 ]
 def printing_bhool_bhulaiya(bhool_bhulaiya, stdscr, path=[]):
     white= curses.color_pair(1)
@@ -21,7 +20,10 @@ def printing_bhool_bhulaiya(bhool_bhulaiya, stdscr, path=[]):
 
     for i, row in enumerate(bhool_bhulaiya):
         for j, value in enumerate(row):
-            stdscr.addstr(i,j,value, white)
+            if (i, j) in path:
+               stdscr.addstr(i, j*2,"X", red) 
+            else:
+                stdscr.addstr(i, j*2,value, white)
 
 def find_start(bhool_bhulaiya, start):
     for i, row in enumerate(bhool_bhulaiya):
@@ -44,17 +46,45 @@ def find_path(bhool_bhulaiya, stdscr):
         current_pos, path = q.get()
         row, col =current_pos
 
+        stdscr.clear()
+        printing_bhool_bhulaiya(bhool_bhulaiya, stdscr, path)
+        stdscr.refresh()
+        time.sleep(0.3)   # pause 0.3 seconds
         if bhool_bhulaiya[row][col] == end:
             return path
+        
+        neighbors = find_neighbors(bhool_bhulaiya, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+            r, c = neighbor
+            if bhool_bhulaiya[r][c] == "|":
+                continue
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor) 
+
+
+def find_neighbors(bhool_bhulaiya, row, col):
+        neighbors = []
+        if row > 0 : #up
+            neighbors.append((row-1, col))
+        if row+1 < len(bhool_bhulaiya): #down
+            neighbors.append((row+1, col))
+        if col > 0 : #left
+            neighbors.append((row, col-1))
+        if col+1 < len(bhool_bhulaiya): #right
+            neighbors.append((row, col+1))
+
+        return neighbors        
+
     
 def main (stdscr):
     curses.init_pair (1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair (1, curses.COLOR_RED, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr(0 , 0)
-    stdscr.refresh()
+    curses.init_pair (2, curses.COLOR_RED, curses.COLOR_BLACK)
+    
+    find_path(bhool_bhulaiya, stdscr)
     stdscr.getch()
-
 
 
 wrapper(main)
